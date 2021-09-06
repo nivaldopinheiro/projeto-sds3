7e9143da1ca92551753e76b4a7f7271c5b25643b
¨nivaldopinheiro¨ <pinheiro.nivaldo37@gamil.com>
2021-09-06 19:30:16 -0300

    Project created

---
+++ backend/_pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,6 @@
+package snippet;
+
+public class Snippet {
+	por favor alguém sabe como deletar um projeto do spring tools, eu sou aprendiz ainda e errei ao escolher o import, ao invés do backend escolhi frontend e quando tento novamente ele diz que ja existe um arquivo rodando
+}
+
