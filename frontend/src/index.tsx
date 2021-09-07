198dc6f670fafbf7cf63dbe3a1c951260f35ec64
¨nivaldopinheiro¨ <pinheiro.nivaldo37@gamil.com>
2021-09-07 13:27:25 -0300

    Static charts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'assets/css/styles.css';
 import App from './App';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
